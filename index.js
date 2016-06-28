3fd91d4d6f23aa0925d454c215c1b32fc3489dc8
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-28 19:04:58 +0000

    add latest

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ var HistorySchema = new Schema({
     term : { type: String, required: true, trim: true },
     time : { type: Date, required: true, trim: true }
 });
+var History = mongoose.model('History',HistorySchema);
+
  
 var search_it = new search('hAesGkyxDUEtkrw+n6hDlxBtoZHRnU5QQ6FJvlKqxhk');
+mongoose.connect('mongodb://db_user:img-search@ds023714.mlab.com:23714/th-img-search-db');
 
  
 app.listen(port, function(){ 
@@ -20,71 +23,51 @@ app.listen(port, function(){
   });
 
 app.get('/api/imagesearch/*', function(req_g,res_g) {
- // https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10
-mongoose.connect('mongodb://db_user:img-search@ds023714.mlab.com:23714/th-img-search-db');
-var History = mongoose.model('History',HistorySchema);
+         // https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10
 
-var history = new History ( {
-    term: req_g.params[0],
-    time: new Date()
-});
-
-history.save( function(error, data){
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(data +  " is saved.");
-    }
-});
-
-
-console.log(req_g.params[0]); 
-search_it.images(req_g.params[0],
-  {top: 5},
-  function(err, results) {
-    if (err) {
-      console.log(err);
-      res_g.status(err.status).end();
-    }
-    else{
-      console.log(util.inspect(results,{colors: true, depth: null}));
-      res_g.json(results);
-    }
-  }
-);
+        var history = new History ( {
+            term: req_g.params[0],
+            time: new Date()
+        });
+        
+        history.save( function(error, data){
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log(data +  " is saved.");
+            }
+        });
+        
+        
+        console.log(req_g.params[0]); 
+        search_it.images(req_g.params[0],
+          {top: 5},
+          function(err, results) {
+            if (err) {
+              console.log(err);
+              res_g.status(err.status).end();
+            }
+            else{
+              console.log(util.inspect(results,{colors: true, depth: null}));
+              res_g.json(results);
+            }
+          }
+        );
 
 });
-
-app.get('/api/imagesearch/*', function(req_g,res_g) {
- // https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10
-console.log(req_g.params[0]);
-//if(req_g.query.offset !== undefined) { top_default = req_g.query.offset }
-//console.log("DEFAULT: "   + req_g.query);
-
-search_it.images(req_g.params[0],
-  {top: 5},
-  function(err, results) {
-     if (err) {
-      console.log(err);
-      res_g.status(err.status).end();
-    }
-    else{
-      console.log(util.inspect(results,{colors: true, depth: null}));
-      res_g.json(results);
-    }
-  }
-);
-});
-
 
 app.get('/api/latest/imagesearch/', function(req_g,res_g) {
+
+  History.find({}, '-_id term time', function(err, latest_data) {
+  if (err) return console.error(err);
+  else{
+    console.log(latest_data);
+    res_g.json(latest_data);
+  }
+});
   
-  res_g.json({
-    term:"lolcats funny",
-    time:"2016-06-28T14:46:50.042Z"
-  });
-   
+
 });
 
 
