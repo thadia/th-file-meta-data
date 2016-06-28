c59c0807aa910e69a6d7feb0b3c1dbbb0a3d6cd8
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-28 17:41:24 +0000

    adding mongoose

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var search = require('bing.search');
 var path = require('path');
 var util = require('util'); 
 var mongoose = require('mongoose');
-
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectID;
+var HistorySchema = new Schema({
+    term : { type: String, required: true, trim: true },
+    time : { type: Date, required: true, trim: true }
+});
  
 var search_it = new search('hAesGkyxDUEtkrw+n6hDlxBtoZHRnU5QQ6FJvlKqxhk');
 
@@ -16,6 +21,22 @@ app.listen(port, function(){
 
 app.get('/api/imagesearch/*', function(req_g,res_g) {
  // https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10
+mongoose.connect('mongodb://db_user:img-search@ds023714.mlab.com:23714/th-img-search-db');
+var History = mongoose.model('History',HistorySchema);
+
+var history = new History ( {
+    term: req_g.params[0],
+    time: new Date()
+});
+
+history.save( function(error, data){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(data +  " is saved.");
+    }
+});
 
 
 console.log(req_g.params[0]); 
