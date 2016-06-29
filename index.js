748068dceb12c44418a81dcd23c51a8000a80852
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-29 18:51:57 +0000

    cleaning up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var server = require('express');
 var port = process.env.PORT || 3500;
 var app = server();
 var path = require('path');
-var util = require('util'); 
 var multer  = require('multer');
 var upload = multer({ dest: './upload' });
 
@@ -11,11 +10,8 @@ app.listen(port, function(){
   });
 
 app.post('/upload', upload.single('file'), function (req, res) {
-  
   console.log("File size" + req.file.size);
   res.json({file_name: req.file.name, size: req.file.size});
-    // req.file is the `avatar` file 
-    // req.body will hold the text fields, if there were any 
 
 });
 
