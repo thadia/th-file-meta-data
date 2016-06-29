ff1bbd8fd56ccae381e1cdbf5e314d94e6678061
Martha Obrinteschi <martha.obrinteschi@gmail.com>
2016-06-29 18:41:31 +0000

    fixing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = server();
 var path = require('path');
 var util = require('util'); 
 var multer  = require('multer');
-var upload = multer({ dest: 'upload/' });
+var upload = multer({ dest: './upload' });
 
 app.listen(port, function(){ 
   console.log('Ready: ' + port);
@@ -12,8 +12,8 @@ app.listen(port, function(){
 
 app.post('/upload', upload.single('file'), function (req, res,next) {
   
-  console.log(req.body);
-  //res.json(req.body);
+  console.log(req.file.size);
+  res.json(req.file.size);
     // req.file is the `avatar` file 
     // req.body will hold the text fields, if there were any 
 
